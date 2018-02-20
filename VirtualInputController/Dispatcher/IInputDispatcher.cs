62a80215046b8a509af172a6463a6571f7408147
Jordan Small <jsmall53@ymail.com>
2018-02-19 20:56:52 -0800

    Added a VirtualInputController.Common project which will house any Interfaces and models that will be needed to utilize the service framework. Added a few basic interfaces to common Added the IInputDispatcher interface which will be in charge of dispatching any inputs needs. This allows multi key inputs to be sent simultaneously to the Win32API wrapper.

---
+++ VirtualInputController/Dispatcher/IInputDispatcher.cs
@@ -0,0 +1,16 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace VirtualInputController.Dispatcher
+{
+    public interface IInputDispatcher
+    {
+        bool SendOneOrMoreInputs(params long[] inputs);
+    }
+}
