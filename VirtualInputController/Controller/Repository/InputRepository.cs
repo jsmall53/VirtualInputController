6e85688bbc0838b74a7f0b22464b36c93dc93486
Jordan Small <jsmall53@ymail.com>
2018-02-21 21:46:41 -0800

    Added Input Controller Added InputRepository, to add something between Controller and repository

---
+++ VirtualInputController/Controller/Repository/InputRepository.cs
@@ -0,0 +1,16 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace VirtualInputController.Controller.Repository
+{
+    public class InputRepository
+    {
+
+    }
+}
